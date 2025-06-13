380cefb1811f58991ea696e67186ebb36a2477d0
AurelienAllenic <aurelien.allenic@gmail.com>
2025-06-13 20:40:53 +0200

    (feat) added products list and product details

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_USER')]
+final class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'app_products_list')]
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_product_show', requirements: ['id' => '\d+'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
