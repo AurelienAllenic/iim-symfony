a18741ae58abebfe05a73ef8050116b9a31ea04b
AurelienAllenic <aurelien.allenic@gmail.com>
2025-06-14 16:09:00 +0200

    (feat) added api platform

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,37 +2,57 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ProductRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'product:item']),
+        new GetCollection(normalizationContext: ['groups' => 'product:list'])
+    ],
+    order: ['updateAt' => 'DESC', 'createdAt' => 'ASC'],
+    paginationEnabled: false,
+)]
 class Product
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['product:list', 'product:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['product:list', 'product:item'])]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Groups(['product:list', 'product:item'])]
     private ?int $price = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['product:list', 'product:item'])]
     private ?string $category = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[Groups(['product:list', 'product:item'])]
     private ?string $description = null;
 
     #[ORM\ManyToOne(inversedBy: 'products')]
+    #[Groups(['product:list', 'product:item'])]
     private ?User $user = null;
 
     #[ORM\Column]
+    #[Groups(['product:list', 'product:item'])]
     private ?\DateTimeImmutable $createdAt = null;
 
     #[ORM\Column]
+    #[Groups(['product:list', 'product:item'])]
     private ?\DateTimeImmutable $updatedAt = null;
 
     public function getId(): ?int
